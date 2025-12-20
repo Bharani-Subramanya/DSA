8b88bd6a1b184c200015594ab97656943a7fabd5
Bharani-Subramanya <navaloorivenkata@gmail.com>
2025-12-20 13:43:50 +0530

    rotate using temparary array is done

--- arrays/rotatearray.java
+++ arrays/rotatearray.java
@@ -7,6 +7,10 @@ public class rotatearray{
         int d=2;
 
         looprotate(arr,d);
+
+        int [] arr2={1,2,3,4,5,6,7};
+        int d2=2;
+        temparray(arr2,d2);
     }
     public static void looprotate(int [] arr, int d)
     {
@@ -21,7 +25,29 @@ public class rotatearray{
             }
             arr[n-1]=fir;
         }
-
         System.out.println(Arrays.toString(arr));
     }
+
+    public static void temparray(int []arr, int d)
+    {
+        int n=arr.length;
+
+        d%=n;
+        int [] temp =new int[n];
+
+        for(int i=0;i<n-d;i++)
+        {
+            temp[i]=arr[d+i];
+        }
+        for(int i=0;i<d;i++)
+        {
+            temp[n-d+i]=arr[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=temp[i];
+        }
+
+        System.out.println(Arrays.toString(arr));
+     }
 }
