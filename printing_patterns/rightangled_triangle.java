94806a2e08b64ca716698315811db770cf99da88
Bharani-Subramanya <navaloorivenkata@gmail.com>
2026-01-04 16:57:11 +0530

    Right Angled Triangle

---
+++ printing_patterns/rightangled_triangle.java
@@ -0,0 +1,22 @@
+package coding.printing_patterns;
+
+import java.util.*;
+
+public class rightangled_triangle {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the value of the row : ");
+
+        int r=sc.nextInt();
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
